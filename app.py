1b678789fe6c41d8d034bd54ac1f77e59fc6c550
alfaznawaz2016-hub <alfaznawaz2016@gmail.com>
2026-03-02 00:30:02 +0600

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,7 @@ def portfolio():
 def contact():
     return render_template('contact.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
